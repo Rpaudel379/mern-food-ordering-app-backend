0f7309f4337ea8ee1729ffb0ca6c77a5804e20a7
Anish Paudel <rpaudel379@gmail.com>
2024-06-09 21:56:17 +0530

    adding view order features

--- src/controllers/OrderController.ts
+++ src/controllers/OrderController.ts
@@ -23,6 +23,19 @@ type CheckoutSessionRequest = {
   restaurantId: string;
 };
 
+const getMyOrders = async (req: Request, res: Response) => {
+  try {
+    const orders = await Order.find({ user: req.userId })
+      .populate("restaurant")
+      .populate("user");
+
+    res.json(orders);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something Went Wrong!" });
+  }
+};
+
 const stripeWebhookHandler = async (req: Request, res: Response) => {
   let event;
 
@@ -167,6 +180,7 @@ const createSession = async (
 };
 
 export default {
+  getMyOrders,
   createCheckoutSession,
   stripeWebhookHandler,
 };
